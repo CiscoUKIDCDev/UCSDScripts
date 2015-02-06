9de6e7742bae3f4e609b92c4069b3447d79f4c77
j051034 <rob.j.edwards@gmail.com>
2015-02-06 19:36:04 +0000

    Refactored existing jobs to use ruby libraries

--- dashing/jobs/vm_by_os.rb
+++ dashing/jobs/vm_by_os.rb
@@ -7,27 +7,40 @@
 # This script queries UCS Director at specific intervals to count the VMs across all clouds
 # and list them by operating system. Takes a while to run.
 
+require 'net/http'
 require 'json'
 
+uri = URI('http://10.52.208.38/app/api/rest?opName=userAPIGetTabularReport&opData=%7Bparam0:%220%22,param1:%22All%20Clouds%22,param2:%22VMS-T0%22%7D')
+
 SCHEDULER.every '120s' do
-	# Lazy way to pull down the REST call - probably should use a library rather than a backtick syscall!
-	response = JSON.parse(`http_proxy="" proxy="" curl -s -X "GET" "http://10.52.208.38/app/api/rest?opName=userAPIGetTabularReport&opData=%7Bparam0:%220%22,param1:%22All%20Clouds%22,param2:%22VMS-T0%22%7D" -H "x-cloupia-request-key: D47D6DD47B99423D9E499848DDF6D0A9"`)
+	# http request
+	req = Net::HTTP::Get.new(uri.request_uri)
 
-	# Store the VMs and status in a hash for sorting later
-	vm_count = Hash.new(0)
-	status = Hash.new(0)
+	# Add API key
+	req.add_field "X-Cloupia-Request-Key", "D47D6DD47B99423D9E499848DDF6D0A9"
 
-	response["serviceResult"]["rows"].each do |vm|
-		vm_count[vm["Guest_OS_Type"]] += 1
+	# Fetch Request
+	res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+		http.request(req)
+	}
+	# If we get a HTTP 200 (OK) response then parse:
+	if (res.code == "200") then
+		# Assume JSON is valid:
+		response = JSON.parse(res.body)
+		# Store the VMs and status in a hash for sorting later
+		vm_count = Hash.new(0)
+		status = Hash.new(0)
+	
+		response["serviceResult"]["rows"].each do |vm|
+			vm_count[vm["Guest_OS_Type"]] += 1
+		end
+	
+	        vm_count.keys.sort_by { |key| vm_count[key] }.reverse.each do |key|
+	        	status[key] = { label: key, value: (vm_count[key].to_i) }
+	        end
+
+		# Send the event to all dashboards
+		send_event('vm_by_os', { items: status.values } )
+	
 	end
-
-        vm_count.keys.sort_by { |key| vm_count[key] }.reverse.each do
-        |key|
-        	status[key] = { label: key, value: (vm_count[key].to_i) }
-        end
-
-	# Send the event to all dashboards
-	send_event('vm_by_os', { items: status.values } )
-
 end
-
